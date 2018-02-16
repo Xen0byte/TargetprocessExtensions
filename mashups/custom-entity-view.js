0d56c6659834d1b1f85ebd10145fcafe08c45e25
Unknown <vatanasescu@outlook.com>
2018-02-16 16:36:59 +0000

    C# Style Brackets

--- mashups/custom-entity-view.js
+++ mashups/custom-entity-view.js
@@ -3,21 +3,29 @@ tau.mashups
     .addDependency('tp/general/view')
     .addDependency('tp3/mashups/topmenu')
     .addDependency('tp3/mashups/popup')
-    .addMashup(function ($, view, topmenu, popup) {
-        view.onRender(function () {
-            var entityIdentifier = "div.view-header__icon em.tau-entity-icon.tau-entity-icon-full";
-            var menuItem = topmenu.addItem({ title: 'Coming Soon™', icon: 'global-settings' });
+    .addMashup(function ($, view, topmenu, popup)
+    {
+        var menuItem = topmenu.addItem({ title: 'Coming Soon™', icon: 'global-settings' });
 
+        view.onRender(function ()
+        {
+            var entityIdentifier = "div.view-header__icon em.tau-entity-icon.tau-entity-icon-full";
             var entityType = $(entityIdentifier).is(":visible") ? document.querySelector(entityIdentifier).textContent : "NONE";
 
-            menuItem.addItem('Customize View').onClick(function () {
-                if (entityType === "Bug") {
+            //show menuitem and do show and hide instead of cerate new
+
+            menuItem.addItem('Customize View').onClick(function ()
+            {
+                if (entityType === "Bug")
+                {
                     popup = new popup('<div style="position: absolute; height: 150px; width: 100%; top: calc((100% - 150px) / 2);">' +
                         '<h1 align="center"><span style="color: #00A591;">Coming Soon™</span></h1>' +
                         '</div>');
                     popup.show();
                 }
-                else {
+                
+                else
+                {
                     popup = new popup('<div style="position: absolute; height: 150px; width: 100%; top: calc((100% - 150px) / 2);">' +
                         '<h1 align="center" style="color: #E94B3C;">This entity is currently not supported.</h1>' +
                         '<h1 align="center" style="color: #E94B3C;">The currently supported entities are: <span style="color: #00A591;">Bugs</span></h1>' +
