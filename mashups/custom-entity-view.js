196d4de7efa10eb4c6882eb7ed096f1897e2c103
Unknown <vatanasescu@outlook.com>
2018-02-22 17:28:07 +0000

    Getting There

--- mashups/custom-entity-view.js
+++ mashups/custom-entity-view.js
@@ -18,18 +18,27 @@ tau.mashups
 
                 if (entityType === "Bug")
                 {
-                    popupInstance = new popup('<div style="position: absolute; height: 150px; width: 100%; top: calc((100% - 150px) / 2);">' +
-                        '<h1 align="center"><span style="color: #00A591;">Coming Soon™</span></h1>' +
-                        '</div>');
+                    popupInstance = new popup('<div id="popup-content" style="overflow: auto; position: absolute; height: 800px; width: 80%; top: calc((100% - 800px) / 2); left: 10%;">' +
+                        '<h2 align="center"><span style="color: #00A591;">Customization Options</span></h2>' + '</div>');
+
+                    var content = $("div#popup-content");
+                    var contentList = $(content).append('<ul id="popup-list" style="list-style-type: none;"></ul>');
+                    var containers = [].slice.call(document.querySelectorAll("div.tau-container.ui-collapsible"));
 
                     popupInstance.show();
-                    var containers = [].slice.call(document.querySelectorAll("div.tau-container.ui-collapsible"));
 
                     containers.forEach(function (container)
                     {
                         console.log(container);
                     });
+
+                    for (i = 0; i < containers.length; i++)
+                    {
+                        var newListItem = $(contentList).append('<li style="box-sizing: border-box; height: calc(45% - 12px); width: calc(25% - 12px); float: left; margin: 3px; border: 3px solid #00A591;"><span>' +
+                            containers[i] + '</span></li>');
+                    }
                 }
+
                 else
                 {
                     popup = new popup('<div style="position: absolute; height: 150px; width: 100%; top: calc((100% - 150px) / 2);">' +
