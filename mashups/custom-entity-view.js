72d19528de0afef8446cc36f80f3a832c18a20cd
Unknown <vatanasescu@outlook.com>
2018-04-11 10:02:46 +0100

    Added Element Processor

--- mashups/custom-entity-view.js
+++ mashups/custom-entity-view.js
@@ -3,44 +3,40 @@ tau.mashups
     .addDependency('tp/general/view')
     .addDependency('tp3/mashups/topmenu')
     .addDependency('tp3/mashups/popup')
-    .addMashup(function ($, view, topmenu, popup)
-    {
+    .addMashup(function ($, view, topmenu, popup) {
         var menuItem = topmenu.addItem({ title: 'Coming Soon™', icon: 'global-settings' });
         var option = menuItem.addItem('Customize View');
 
-        view.onRender(function ()
-        {
+        view.onRender(function () {
             var entityIdentifier = "div.view-header__icon em.tau-entity-icon.tau-entity-icon-full";
 
-            option.onClick(function ()
-            {
+            option.onClick(function () {
                 var entityType = $(entityIdentifier).is(":visible") ? document.querySelector(entityIdentifier).textContent : "NONE";
 
-                if (entityType === "Bug")
-                {
+                if (entityType === "Bug") {
                     popupInstance = new popup('<div id="popup-content" style="overflow: auto; position: absolute; height: 800px; width: 80%; top: calc((100% - 800px) / 2); left: 10%;">' +
                         '<h2 align="center"><span style="color: #00A591;">Customization Options</span></h2>' + '</div>');
 
                     var content = $("div#popup-content");
-                    var contentList = $(content).append('<ul id="popup-list" style="list-style-type: none;"></ul>');
                     var containers = [].slice.call(document.querySelectorAll("div.tau-container.ui-collapsible"));
 
                     popupInstance.show();
 
-                    containers.forEach(function (container)
-                    {
+                    containers.forEach(function (container) {
                         console.log(container);
                     });
 
-                    for (i = 0; i < containers.length; i++)
-                    {
-                        var newListItem = $(contentList).append('<li style="overflow: auto; box-sizing: border-box; height: calc(45% - 12px); width: calc(25% - 12px);' +
-                            'float: left; margin: 3px; padding: 5px; border: 3px solid #00A591;"><span>' + containers[i].innerHTML + '</span></li>');
+                    for (i = 0; i < containers.length; i++) {
+                        console.log("container #" + i + ": " + containers[i].innerHTML);
+
+                        var newItem = '<li id="' + 'newitem_' + i + '" style="overflow: auto; box-sizing: border-box; height: calc(45% - 12px); width: calc(25% - 12px);' +
+                            'float: left; margin: 3px; padding: 5px; border: 3px solid #00A591;"><span>' + containers[i].innerHTML + '</span></li>';
+
+                        $(content).append(parseElement(newItem, "newitem_" + i));
                     }
                 }
 
-                else
-                {
+                else {
                     popup = new popup('<div style="position: absolute; height: 150px; width: 100%; top: calc((100% - 150px) / 2);">' +
                         '<h1 align="center" style="color: #E94B3C;">This entity is currently not supported.</h1>' +
                         '<h1 align="center" style="color: #E94B3C;">The currently supported entities are: <span style="color: #00A591;">Bugs</span></h1>' +
@@ -50,6 +46,19 @@ tau.mashups
                 }
             });
         });
+
+        // THIS IS WHERE THE MAGIC HAPPENS
+        function parseElement(element, id) {
+            $(document.body).append(element);
+
+            var parsedElement = $(element).each(function () {
+                console.log($(this));
+            });
+
+            $("#" + id).remove();
+
+            return parsedElement;
+        }
     });
 
 // Dependencies Explained Here: https://dev.targetprocess.com/v1.0/docs/dependencies
